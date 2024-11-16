8ecd6876d30cf23d0cb1f5fc63ce0c8419da8d17
chaohui997 <chaohui997@gmail.com>
2024-11-16 14:42:09 +0800

    Update nillion.sh

--- nillion.sh
+++ nillion.sh
@@ -9,7 +9,7 @@ show_menu() {
 install_docker() {
     if ! command -v docker &> /dev/null; then
         echo "未检测到 Docker，正在安装 Docker..."
-        
+
         # 更新 apt 包索引并安装依赖
         sudo apt update && sudo apt install ca-certificates curl gnupg lsb-release -y
         
@@ -18,9 +18,7 @@ install_docker() {
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
         
         # 设置 Docker 的 APT 源
-        echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         
         # 安装 Docker 引擎
         sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -28,7 +26,7 @@ install_docker() {
         # 启动并启用 Docker 服务
         sudo systemctl start docker
         sudo systemctl enable docker
-        
+
         echo "Docker 安装完成！"
     else
         echo "Docker 已安装，跳过安装步骤。"
